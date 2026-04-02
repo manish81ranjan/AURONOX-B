7edd3585338dec511fb997b57a0ecc29587e41ee
Manish-2410 <vkdeoghar81@gmail.com>
2026-04-02 22:21:44 +0530

    Update config.py

--- config.py
+++ config.py
@@ -5,6 +5,12 @@ load_dotenv()
 
 class Config:
     SECRET_KEY = os.getenv("SECRET_KEY", "auronox-secret-key")
+
     MONGO_URI = os.getenv("MONGO_URI")
     DB_NAME = os.getenv("DB_NAME", "auronox_db")
-    FRONTEND_ORIGIN = os.getenv("FRONTEND_ORIGIN", "http://127.0.0.1:5501")
+
+    # ✅ production frontend (Vercel)
+    FRONTEND_ORIGIN = os.getenv(
+        "FRONTEND_ORIGIN",
+        "https://auronox.vercel.app"
+    )
